adc371b74e5d8eae9b65bdef8ac06cbbac3bf782
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-04 01:18:41 +0300

    add article test

--- app/news_test.py
+++ app/news_test.py
@@ -26,6 +26,32 @@ class SourcesTest(unittest.TestCase):
         self.assertEquals(self.new_source.url,'https://google.com')
         self.assertEquals(self.new_source.category,'sports')
         self.assertEquals(self.new_source.country,'ke')
+        
+        
+class TestArticles(unittest.TestCase):
+    '''
+    Test class to test the behavior of the articles class
+    '''
+    def setUp(self):
+        '''
+        Test class to run before other tests
+        '''
+        self.new_article = Articles('Rose','Musk does it again','News of Elon Musk buying twitter surprising many','https://google.com','https://google.com/images','2022-05-12T13:31:03Z')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article,Articles))
+    
+    def test_to_check_instance_variables(self):
+        self.assertEquals(self.new_article.author,'evelyne')
+        self.assertEquals(self.new_article.title,'Musk does it again')
+        self.assertEquals(self.new_article.description,'News of Elon Musk buying twitter surprising many')
+        self.assertEquals(self.new_article.url,'https://google.com')
+        self.assertEquals(self.new_article.urlToImage,'https://google.com/images')
+        self.assertEquals(self.new_article.publishedAt,'2022-05-12T13:31:03Z')        
+        
+        
+        
+        
               
         
    
