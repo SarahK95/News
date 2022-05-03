b7051aca7ab923c9fb6e3bba285d4df263c54d8e
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-04 00:09:16 +0300

    add news API key

--- app/views.py
+++ app/views.py
@@ -9,14 +9,14 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    title = 'Home - Welcome to The best News Website Online'
+    return render_template('index.html', title = title)
 
 
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
+@app.route('/news/<news_id>')
+def news(news_id):
 
     '''
-    View movie page function that returns the movie details page and its data
+    View news page function that returns the news details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    return render_template('news.html',id = news_id)
