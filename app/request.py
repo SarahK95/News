43e3e33238bba9c2bd5b97000cf1ec91e01202e9
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-04 01:49:47 +0300

    add get sourcesfunction

--- app/request.py
+++ app/request.py
@@ -17,4 +17,24 @@ def configure_request(app):
     api_key =app.config['NEWS_API_KEY']
     base_url=app.config['NEWS_SOURCES_BASE_URL']
     article_url=app.config['NEWS_ARTICLES_BASE_URL']
+    
+    
 
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['results']:
+            sources_results_list = get_sources_response['results']
+            sources_results = process_sources(sources_results_list)
+
+
+    return sources_results
